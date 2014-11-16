2c15561b714180d0ba91bc47866eeb3849e76c85
Max Terry <mhterry@gmail.com>
2014-11-16 18:50:46 -0500

    Fix show/hide commentary text

--- scripts/commentary.js
+++ scripts/commentary.js
@@ -1,12 +1,12 @@
 Echo.commentary = function(self) {
 	var text = self['textContent']? 'textContent' : 'innerHTML';
 	var mode = self[text].split(/\s/)[0] || "Show";
-	self[text] = (mode == "Show"? "Hide" : "Show") + " Design Commentary";
+	self[text] = (mode == "Show"? "Hide" : "Show") + " Commentary";
 	var comments = document.getElementsByTagName('p');
 	for (var i=0, l=comments.length; i<l; i++) {
 		if (comments[i].className == 'comment') {
 			comments[i].style.display = mode == "Show"? 'block' : 'none';
 		}
 	}
-	if (mode == "Hide") document.location.hash = ''
+	if (mode == "Hide") document.location.hash = '';
 }
